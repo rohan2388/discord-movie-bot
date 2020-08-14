93aaea05bb40a1c225758709e7d33027da07dab3
Rohan Das <rohan@karbonblack.com>
2020-08-15 03:31:01 +0530

    Mongo auth

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,10 @@ const { Schema, model } = mongoose;
 const HOST = process.env.MONGO_HOST || 'localhost';
 const PORT = process.env.MONGO_PORT || 27017;
 const DB  	= process.env.MONGO_DB || 'moviebot';
+const USER = process.env.MONGO_USER;
+const PASS = process.env.MONGO_PASSWORD;
 
-mongoose.connect(`mongodb://${HOST}:${PORT}/${DB}`, {
+mongoose.connect(`mongodb://${USER}:${PASS}@${HOST}:${PORT}/${DB}`, {
 	useNewUrlParser: true,
 	useUnifiedTopology:true,
 	useCreateIndex: true,
